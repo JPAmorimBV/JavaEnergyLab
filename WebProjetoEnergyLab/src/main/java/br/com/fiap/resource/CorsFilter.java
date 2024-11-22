0fe06ae1c338c7213ab2c4b8d0917f0692d0a49b
J.P. Amorim <166668681+JPAmorimBV@users.noreply.github.com>
2024-11-22 18:28:29 -0300

    Ajustando método da API e a classe CorsFilter

--- WebProjetoEnergyLab/src/main/java/br/com/fiap/resource/CorsFilter.java
+++ WebProjetoEnergyLab/src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -10,13 +10,18 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
-	@Override
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-			throws IOException {
-		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-	}
-
+   @Override
+   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+           throws IOException {
+       
+       responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+       responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+       responseContext.getHeaders().add("Access-Control-Allow-Headers", 
+           "origin, content-type, accept, authorization, x-requested-with");
+       responseContext.getHeaders().add("Access-Control-Allow-Methods", 
+           "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+       responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+       responseContext.getHeaders().add("Access-Control-Expose-Headers", 
+           "origin, content-type, accept, authorization, x-requested-with");
+   }
 }
--- WebProjetoEnergyLab/src/main/java/br/com/fiap/resource/UsuarioResource.java
+++ WebProjetoEnergyLab/src/main/java/br/com/fiap/resource/UsuarioResource.java
@@ -5,11 +5,10 @@ import java.sql.SQLException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;  // Import correto
+import javax.ws.rs.PathParam;  
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -67,7 +66,7 @@ public class UsuarioResource {
         }
     }
 
-    @GET
+    @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
